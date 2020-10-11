93acd22e595af3e5e8c67c065bd1a0a9a38ac82f
ashu11-01 <writetoashutosh@gmail.com>
2020-10-11 20:10:11 +0530

    Added Bootstrap to index.html

--- src/component/content.js
+++ src/component/content.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 function Content(){
-    const message = "Developed by: Ashutosh Kumar"
     return(
-        <div style={{backgroundColor: "#000000", color: "#d3d3d3", padding:20, textAlign: "center"}}>
+        <div className="container" style={{backgroundColor: "#000000", color: "#d3d3d3", padding:20, textAlign: "center"}}>
            <h2>Main Content Here</h2>
         </div>
     )
--- src/component/header.js
+++ src/component/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(){
     const appName = "MCDM website"
     return(
-        <div style={{backgroundColor: "#3f3f3f", color: "#d3d3d3", padding:10, textAlign: "center"}}>
+        <div className="container-fluid" style={{backgroundColor: "#3f3f3f", color: "#d3d3d3", padding:20}}>
             {/* <img src={logo} alt={appName} width={80} height={40}/> */}
             <h1>{appName}</h1>
         </div>
