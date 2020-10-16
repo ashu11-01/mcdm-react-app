31214b4d0566237c40288a3c9c1783bf5047ca1c
ashu11-01 <writetoashutosh@gmail.com>
2020-10-16 15:01:23 +0530

    fixed handleFieldChange. loop till alternativeList.length

--- src/component/MethodInput.js
+++ src/component/MethodInput.js
@@ -7,25 +7,18 @@ class MethodInput extends React.Component{
             alternatives: [],
             alternativeList : []
         }
-        // this.state={}
         this.addNewAlternative = this.addNewAlternative.bind(this)
         this.handleFieldChange = this.handleFieldChange.bind(this)
     }
     
     handleFieldChange(fieldId, value){
-       
-        // this.setState({ [fieldId] : value})
         const array = this.state.alternatives
-        for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < this.state.alternativeList.length; index++) {
             if(index===fieldId){
                 array[index] = value
-            }
-                // array[index] += value
-            
+            }            
         }
-        console.log(array)
         this.setState({alternatives : array})
-        // console.log(this.state)
     }
 
     addNewAlternative(){
@@ -35,17 +28,8 @@ class MethodInput extends React.Component{
             alternativeList : prevState.alternativeList.concat(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>)
         })
         })
-        // console.log(this.state)
-        // console.log(alternativeList)
     }
     render(){
-
-        // let alternativeList = []
-        // for (let index = 0; index < 4; index++) {
-        //     alternativeList.push(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>)
-            
-        // }
-        // console.log(this.state)
         return(
             
         <div className="container">
@@ -54,10 +38,6 @@ class MethodInput extends React.Component{
             <button onClick={this.addNewAlternative}>Add new Alternative</button>
             <br/>
            {this.state.alternativeList} 
-           {/* <Alternative id="1" onChange={this.handleFieldChange} value={this.state[1]}/>
-           <Alternative id="2" onChange={this.handleFieldChange} value={this.state[2]}/> */}
-           {/* {alternativeList} */}
-        {/* <p>{JSON.stringify(this.state)}</p> */}
         </div>
     )
     }
