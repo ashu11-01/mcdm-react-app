4b802124cb2a4660fa83ed89518ce81101e8df18
ashu11-01 <writetoashutosh@gmail.com>
2020-10-16 15:47:42 +0530

    added removeAlternative method. Can dynamically remove alternatives

--- src/component/MethodInput.js
+++ src/component/MethodInput.js
@@ -9,6 +9,7 @@ class MethodInput extends React.Component{
         }
         this.addNewAlternative = this.addNewAlternative.bind(this)
         this.handleFieldChange = this.handleFieldChange.bind(this)
+        this.removeAlternative = this.removeAlternative.bind(this)
     }
     
     handleFieldChange(fieldId, value){
@@ -23,21 +24,48 @@ class MethodInput extends React.Component{
 
     addNewAlternative(){
         const index = this.state.alternativeList.length || 0
+        const item = <div key={index}>
+            <Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>
+            <br/>
+        </div>
         this.setState(prevState =>{
             return ({
-            alternativeList : prevState.alternativeList.concat(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>)
+            alternatives : prevState.alternatives.concat(""),
+            alternativeList : prevState.alternativeList.concat(item)
         })
         })
     }
-    render(){
-        return(
+
+
+    removeAlternative(){
+        // const index = this.state.alternativeList.length -1 || 0
+        if(this.state.alternativeList.length===1){
+            alert("Must be atleast one alternative")
+            return
+        }
             
+        this.setState(prevState =>{
+            return ({
+                alternatives : prevState.alternatives.concat(prevState.alternatives.pop()),
+            alternativeList : prevState.alternativeList.concat(prevState.alternativeList.pop())
+        })
+        })
+    }
+
+
+    render(){
+
+        return(
         <div className="container">
            <h1>{this.props.match.params.method}</h1>
              <br/>
-            <button onClick={this.addNewAlternative}>Add new Alternative</button>
+             <h4>Alternatives</h4>
+             {this.state.alternativeList} <br/>
+            <button onClick={this.addNewAlternative}>+</button>
+            {this.state.alternativeList.length >0 ? <button onClick={this.removeAlternative}>-</button> : null }
             <br/>
-           {this.state.alternativeList} 
+        <p>{JSON.stringify(this.state)}</p>
+        <p>{JSON.stringify(this.state.alternativeList.length)}</p>
         </div>
     )
     }
