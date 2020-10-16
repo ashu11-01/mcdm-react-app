dbc4c58ff8f858cbf0a96a1fe074d3e5c5a01620
ashu11-01 <writetoashutosh@gmail.com>
2020-10-16 14:58:57 +0530

    alternative component dynamically adding

--- src/component/MethodInput.js
+++ src/component/MethodInput.js
@@ -4,6 +4,8 @@ class MethodInput extends React.Component{
     constructor(){
         super()
         this.state={
+            alternatives: [],
+            alternativeList : []
         }
         // this.state={}
         this.addNewAlternative = this.addNewAlternative.bind(this)
@@ -11,50 +13,51 @@ class MethodInput extends React.Component{
     }
     
     handleFieldChange(fieldId, value){
-        // const alt = this.state.alternatives
-        // this.setState(prevState =>{
-        //     const updatedAlternatives = prevState.alternatives.map((alternative,index) => {
-        //         if( index === fieldId)
-        //             alternative=value
-        //         return alternative
-        //     })
-        //     return updatedAlternatives
-        // })
-        this.setState({ [fieldId] : value})
-        // console.log(fieldId,value)
-        // this.state.alternativeList[index].props.
+       
+        // this.setState({ [fieldId] : value})
+        const array = this.state.alternatives
+        for (let index = 0; index < 4; index++) {
+            if(index===fieldId){
+                array[index] = value
+            }
+                // array[index] += value
+            
+        }
+        console.log(array)
+        this.setState({alternatives : array})
+        // console.log(this.state)
     }
 
     addNewAlternative(){
-        const alternativeList = this.state.alternativeList
-        const index = alternativeList.length
-        this.setState({
-            alternativeList : alternativeList.concat(
-            <Alternative key={index} name={index} value="" onChange={this.handleFieldChange}/>
-            )}
-        )
-        console.log(alternativeList)
+        const index = this.state.alternativeList.length || 0
+        this.setState(prevState =>{
+            return ({
+            alternativeList : prevState.alternativeList.concat(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>)
+        })
+        })
+        // console.log(this.state)
+        // console.log(alternativeList)
     }
     render(){
 
-        let alternativeList = []
-        for (let index = 0; index < 4; index++) {
-            alternativeList.push(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state[index]}/>)
+        // let alternativeList = []
+        // for (let index = 0; index < 4; index++) {
+        //     alternativeList.push(<Alternative key={index} id={index} onChange={this.handleFieldChange} value={this.state.alternatives[index]}/>)
             
-        }
-        console.log(this.state)
+        // }
+        // console.log(this.state)
         return(
             
         <div className="container">
            <h1>{this.props.match.params.method}</h1>
-            {/* <br/>
+             <br/>
             <button onClick={this.addNewAlternative}>Add new Alternative</button>
             <br/>
-           {this.state.alternativeList} */}
+           {this.state.alternativeList} 
            {/* <Alternative id="1" onChange={this.handleFieldChange} value={this.state[1]}/>
            <Alternative id="2" onChange={this.handleFieldChange} value={this.state[2]}/> */}
-           {alternativeList}
-        <p>{JSON.stringify(this.state)}</p>
+           {/* {alternativeList} */}
+        {/* <p>{JSON.stringify(this.state)}</p> */}
         </div>
     )
     }
